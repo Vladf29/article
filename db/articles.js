1f1a0534aada0793056529573a27e1696741f7c8
Vladf29 <boxforemailand@gmail.com>
2018-05-02 00:15:36 +0300

    schema article

---
+++ db/articles.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const articleSchema = new Schema({
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    comment: [{
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    }],
+    liked: [{
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    }],
+    content: String
+});
+
+const Article = mongoose.model('article', articleSchema);
+
+module.exports = Article;
