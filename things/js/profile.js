4168f04b4f9251a0f9fbd6bd14e3d1b4293db6d1
Vladf29 <boxforemailand@gmail.com>
2018-05-04 20:10:34 +0300

    profile about me front

--- things/js/profile.js
+++ things/js/profile.js
@@ -80,13 +80,85 @@ $('.js-upload-form-url').submit(function (event) {
     elemPreview.find('.preview-c').append(`<img src='${url}'>`);
 });
 
+$('.js-edit').on('change', async function () {
+    try {
+        const edit = $(this).attr('data-edit');
+        const val = $(this).val();
+        if (!val) return;
+
+        const obj = {}
+        obj[edit] = val;
+
+        const opts = {
+            url: '/test',
+            method: 'PUT',
+            header: ['Content-Type', 'application/json'],
+            data: JSON.stringify(obj)
+        }
+
+        const result = await httpRequest(opts);
+        console.log(result);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+$('.js-edit-btn').click(function () {
+    const ok = $(this).closest('.user-profile__c').find('.about-block .about-block__c[data-action=ok]');
+    const edit = $(this).closest('.user-profile__c').find('.about-block .about-block__c[data-action=edit]');
+
+    const val = ok.find('p').text();
+    edit.find('textarea').val(val);
+    ok.attr('data-state', 'hidden');
+    edit.attr('data-state', 'show');
+
+    $(this).attr('data-state', 'hidden');
+    $('.js-edit-btn-ok').attr('data-state', 'show');
+
+    edit.find('textarea').focus();
+});
+
+$('.js-edit-btn-ok').click(function () {
+    const ok = $(this).closest('.user-profile__c').find('.about-block .about-block__c[data-action=ok]');
+    const edit = $(this).closest('.user-profile__c').find('.about-block .about-block__c[data-action=edit]');
+
+    const val = edit.find('textarea').val();
+    ok.find('p').text(val);
+    edit.attr('data-state', 'hidden');
+    ok.attr('data-state', 'show');
+
+    $('.js-edit-btn').attr('data-state', 'show');
+    $(this).attr('data-state', 'hidden');
+
+    sendAboutMe(val);
+});
+
 /*
  * Isn't ready yet!!!!
  */
+
+async function sendAboutMe(val) {
+    try {
+        const opts = {
+            url: '/test',
+            method: 'PUT',
+            header: ['Content-Type', 'application/json'],
+            data: JSON.stringify({
+                aboutMe: val
+            })
+        }
+
+        const result = await httpRequest(opts);
+        console.log(result);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 async function sendImg() {
     try {
         const img = document.getElementById('userImg');
-        if (img.value === '') return;
+        // if (img.value === '') return;
 
         const formData = new FormData();
         formData.append('img', img.files[0]);
@@ -103,11 +175,10 @@ async function sendImg() {
     }
 }
 
-
 async function sendUrlImg(event) {
     try {
         const url = $('#userURLImg').val();
-        if (url === '') return;
+        // if (url === '') return;
 
         const opts = {
             url: '/test',
